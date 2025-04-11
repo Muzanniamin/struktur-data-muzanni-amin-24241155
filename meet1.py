0c93a78bca0672d2e6e00424b1bb3b5b8a283d22
Muzanniamin <muzaniamin@gmail.com>
2025-04-11 10:08:56 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,41 @@
+#perbandingan kemungkinan nilai = true/salah (bolean)
+
+#nim:24241155
+
+#hasil <
+a = 24
+b = 55
+hasil = a < b
+print(hasil)
+
+#hasil >
+a = 24
+b = 55
+hasil = a > b
+print(hasil)
+
+#hasil <=
+a = 24
+b = 55
+hasil = a <= b
+print(hasil)
+
+#hasil >=
+a = 24
+b = 55
+hasil = a >= b
+print(hasil)
+
+#hasil !=
+a = 24
+b = 55
+hasil = a != b
+print(hasil)
+
+#hasil not
+a = 24
+b = 55
+if not a != b:
+    print("Nilai a sama dengn nlai b")
+else:
+    print("Nilai a tidak sama nilai b")
